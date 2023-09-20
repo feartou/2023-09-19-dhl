98670b30aeba741a891622e34daf4c9cf17d89bd
feartou <mickael.thouvenin@gmail.com>
2023-09-20 10:17:55 +0100

    console log

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-function initJs(){
+function initJs(color){
     var jsLoadedNode=document.querySelector("#isjsloaded");
     jsLoadedNode.innerHTML='<b>Js Chargé</b>';
     jsLoadedNode.classList.add('jsCharger');
+    console.log('toto');
+    console.warn('ejj');
+    console.error('enfdn');
+    console.trace('kfnf');
 }
 
 initJs();
