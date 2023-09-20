7afd8513b49c07f3b988f08309d7ee85ca5cc2e1
feartou <mickael.thouvenin@gmail.com>
2023-09-20 09:28:02 +0100

    modification contenu + css classname insert

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
-document.getElementById("nepascliquer").addEventListener('click',function(){
-    alert("coucou");
-});
+var jsLoadedNode=document.querySelector("#isjsloaded")
+jsLoadedNode.innerHTML='<h1>Js Chargé</h1>'
+jsLoadedNode.classList.add('jsCharger')
