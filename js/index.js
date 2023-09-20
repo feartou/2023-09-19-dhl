f0fac24bc095d5403861b2a3250c15056007c3c1
feartou <mickael.thouvenin@gmail.com>
2023-09-20 15:09:54 +0100

    cast et submit form

--- js/index.js
+++ js/index.js
@@ -3,25 +3,48 @@
  * @param {string} color chaine de valeur de couleur css
  * @returns {undefined} pas de retour
  */
-function changePreHeader(color){
-    var jsLoadedNode=document.querySelector("#isjsloaded");
-    jsLoadedNode.innerHTML='<b>Js Chargé</b>';
-    //jsLoadedNode.classList.add('jsCharger');
-    jsLoadedNode.style.color = color;
+function changePreHeader(color) {
+  var jsLoadedNode = document.querySelector("#isjsloaded");
+  jsLoadedNode.innerHTML = "<b>Js Chargé</b>";
+  //jsLoadedNode.classList.add('jsCharger'); pour ajouter une classe.
+  jsLoadedNode.style.color = color;
 }
 
 /**
  * Appel la fonction changePreHeader lors de l'initialisation de la page.
- * @param {string} color 
+ * @param {string} color
  */
-function initJs(color){
-    changePreHeader(color);
-    document.querySelector('#nepascliquer').addEventListener('click', function (evt) {
-        changePreHeader('green');
-        console.log(evt);
+function initJs(color) {
+  changePreHeader(color);
+  document
+    .querySelector("#nepascliquer")
+    .addEventListener("click", function (evt) {
+      changePreHeader("green");
+      console.log(evt);
     });
+  /**
+   * fonction de gestion de soumission de formulaire de preventdefault
+   * @param {SubmitEvent} evt
+   */
+  function onformsubmit(evt) {
+    evt.preventDefault();
+    console.log(evt);
+    var meme={
+        texte:evt.target['meme_text'].value,
+        x:Number(evt.target['meme_x'].value),
+        y:Number(evt.target['meme_y'].value),
+        taille:Number(evt.target['meme_taille'].value),
+        fontWeight:evt.target['meme_fontWeight'].value,
+        color:evt.target['meme_color'].value
+    }
+    console.log(meme);
+  }
+  document.forms["meme_form"].addEventListener("submit", onformsubmit);
 }
 
-document.addEventListener('DOMContentLoaded', function(){
-    initJs('tomato');
+/**
+ * Evenement de verification du chargement du dom
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  initJs("tomato");
 });
