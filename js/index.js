22877fe9bc2caa1bc41b8e02329a3ce737badf05
feartou <mickael.thouvenin@gmail.com>
2023-09-20 12:28:55 +0100

    gestion event dom load

--- js/index.js
+++ js/index.js
@@ -3,10 +3,25 @@
  * @param {string} color chaine de valeur de couleur css
  * @returns {undefined} pas de retour
  */
-function initJs(color){
+function changePreHeader(color){
     var jsLoadedNode=document.querySelector("#isjsloaded");
     jsLoadedNode.innerHTML='<b>Js Chargé</b>';
-    jsLoadedNode.classList.add('jsCharger');
+    //jsLoadedNode.classList.add('jsCharger');
+    jsLoadedNode.style.color = color;
 }
 
-initJs();
+/**
+ * Appel la fonction changePreHeader lors de l'initialisation de la page.
+ * @param {string} color 
+ */
+function initJs(color){
+    changePreHeader(color);
+    document.querySelector('#nepascliquer').addEventListener('click', function (evt) {
+        changePreHeader('green');
+        console.log(evt);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    initJs('tomato');
+});
